3eb0a0a4ad0e270d3aa13039a935017e4e74c4da
bsaglam <burcu.saglam001@gmail.com>
2020-09-29 10:20:07 +0300

    import alanı düzeltildi

--- ReactTutorial/reactapp/src/index.js
+++ ReactTutorial/reactapp/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { UserProvider } from '.npm install react-pose/Context';
+import { UserProvider } from './Context';
 
 
 ReactDOM.render(
